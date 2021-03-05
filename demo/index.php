c379f92b7e72573da9f00106b43166546f9923a6
Noreh AD <akinoreh@gmail.com>
2021-03-05 16:54:33 +0300

    .

--- demo/index.php
+++ demo/index.php
@@ -2,7 +2,7 @@
 
 include "../src/HTMLElement.php";
 
-$list = new HTMLElement("ul", ["id"=>"mylist", "class"=>"fancy-list"], [
+$list = new HTMLElement("ul", ["id" => "mylist", "class" => "fancy-list"], [
     new HTMLElement("li", null, "Item 1"),
     new HTMLElement("li", null, "Item 2"),
     new HTMLElement("li", null, "Item 3"),
@@ -11,7 +11,7 @@ $list->append(new HTMLElement("li", null, "Item 4"));
 $list->output();
 
 // or alternatively
-$list = elem("ul", ["id"=>"mylist", "class"=>"fancy-list"], [
+$list = elem("ul", ["id" => "mylist", "class" => "fancy-list"], [
     elem("li", null, "Item 1"),
     elem("li", null, "Item 2"),
     elem("li", null, "Item 3"),
